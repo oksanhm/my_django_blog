c383bc8b1f3be7a9606229c740e6ebc2c0354ba2
Oksana <oksana.kashina.01@mail.ru>
2021-10-23 15:28:34 +0300

    bootstrap -> django blog

---
+++ blog/myblog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+
+
+class Post(models.Model):
+    h1 = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    url = models.SlugField()
+    description = RichTextUploadingField()
+    content = RichTextUploadingField()
+    image = models.ImageField()
+    created_at = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_name')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.text
+
+
+'''
+ипортируем User - это уже готовый класс в django для Пользователей. С его помощью легко реализовать Регистрацию и Авторизацию. Об этом будет позже. Но по сути это такая же модель.
+
+Затем мы ипортируем timezone - это нам надо для фиксации времени создания поста.
+
+Потом мы создаем класс Post и наследуем его от класса Model.
+'''
